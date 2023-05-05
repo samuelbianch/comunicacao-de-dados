76f62fdd6ba2d1ad9788a5367dbd436ae4ef616c
samuelbianch <samuelbianch38@gmail.com>
2023-05-05 18:45:47 -0300

    Add: info sobre metodos

--- check_sum.py
+++ check_sum.py
@@ -1,13 +1,17 @@
 
 class Verificador():
+    """Classe responsável por verificar se a entrada é a mesma da saída"""
+
     def __init__(self, entrada):
+        """Método construtor"""
         self.entrada = entrada
 
     def exibe_entrada(self):
+        """Exibe a mensagem que o usuário digitou sem nenhuma verificação"""
         print("Mensagem de entrada: " + self.entrada)
 
     def criptografa_entrada(self):
-        """Varre a lista e a guarda com o valor unicode (ASCI) da entrada"""
+        """Varre a entrada e a guarda uma lista com o valor unicode (ASCI) da entrada"""
         lista = []
         for i in str(self.entrada):
             lista.append(ord(i))
@@ -15,6 +19,7 @@ class Verificador():
         return lista
 
     def soma_entrada(self):
+        """Faz um somatório total com os inteiros da lista no valor de entrada"""
         lista = self.criptografa_entrada()
         soma = 0
         for i in lista:
@@ -23,6 +28,7 @@ class Verificador():
         return soma
     
     def soma_saida(self):
+        """Faz a soma da lista que foi preenchida para a saída"""
         lista = self.descriptografa_entrada()
         soma = 0
         for i in lista:
@@ -31,6 +37,7 @@ class Verificador():
         return soma
     
     def descriptografa_entrada(self):
+        """Peda os valores UNICODE e reverte para string"""
         lista = self.criptografa_entrada()
         lista_saida = []
         print("Mensagem de saida: ", end="")
@@ -41,12 +48,14 @@ class Verificador():
         return lista_saida
 
     def check_sum(self):
+        """Faz a verificação se a entrada e a saída possuem a mesma soma de valores UNICODE"""
         if self.soma_entrada() == self.soma_saida():
             return "\nTudo ok"
         
         return "\nERRO!"
     
     def analisa(self):
+        """Chama os métodos"""
         self.exibe_entrada()
         print(self.check_sum())
 
